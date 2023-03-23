5d6d2cfa7e631203c0cad319374518cabb81420d
ne0nraver <art24896@gmail.com>
2023-03-23 20:00:22 +0300

    work in class

---
+++ src/lesson5/BitwiseExample.java
@@ -0,0 +1,11 @@
+package lesson5;
+
+public class BitwiseExample {
+    public static void main(String[] args) {
+        int i1 = 0b1001;
+        int i2 = 0b1010;
+        System.out.println(Integer.toBinaryString(i1 & i2));
+        System.out.println(Integer.toBinaryString(i1 | i2));
+        System.out.println(Integer.toBinaryString(i1 ^ i2));
+    }
+}
