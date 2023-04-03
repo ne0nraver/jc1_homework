1e262ef9f0b781ae7026ce4e993edd91a60598f2
ne0nraver <art24896@gmail.com>
2023-04-03 21:32:24 +0300

    work in class

---
+++ src/lesson8/MathExample.java
@@ -0,0 +1,12 @@
+package lesson8;
+
+import lesson8.util.StringUtil;
+
+public class MathExample {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(9.0));
+        System.out.println(Math.pow(9,2));
+
+        System.out.println(StringUtil.multiplyString("*",10));
+    }
+}
