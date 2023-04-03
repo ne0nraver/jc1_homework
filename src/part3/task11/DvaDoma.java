6f0f8ba11f24cf3952a63ff1daf197d35162a769
ne0nraver <art24896@gmail.com>
2023-04-03 14:25:54 +0300

    homework

--- src/part3/task11/DvaDoma.java
+++ src/part3/task11/DvaDoma.java
@@ -1,11 +1,9 @@
 package part3.task11;
 
-import java.util.Scanner;
 
 public class DvaDoma {
     public static void main(String[] args) {
         int a,b,c,d,e,f;
-        Scanner sc = new Scanner(System.in);
         a = 12;
         b = 8;
         c = 7;
